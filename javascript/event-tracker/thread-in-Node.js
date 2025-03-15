416f6efddb67a990a90a06ac5d02036b044d8ebd
indresh535 <indreshindresh535@gmail.com>
2025-03-15 23:50:27 +0530

    python road map

--- javascript/event-tracker/thread-in-Node.js
+++ javascript/event-tracker/thread-in-Node.js
@@ -20,7 +20,7 @@ function readFileNonBlocking() {
 // Simulate a blocking operation (e.g., a long computation)
 function blockingOperation() {
   let sum = 0;
-  console.log('Starting blocking on...');
+  console.log('Starting blocking operation...');
   for (let i = 0; i < 1e9; i++) {
     sum += i;
   }
