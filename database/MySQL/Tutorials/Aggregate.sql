13d128c951668ec55bc7868030eeaedc23f4751f
indresh535 <indreshindresh535@gmail.com>
2025-03-21 00:21:57 +0530

    sql advance concepts

---
+++ database/MySQL/Tutorials/Aggregate.sql
@@ -0,0 +1,10 @@
+/*
+Title: MySQL Aggregate Functions
+
+Aggregate Functions
+The aggregate function in advanced SQL summarizes data by performing calculations on 
+groups of values and returning a single result. It is often used with the GROUP BY clause to categorize 
+and analyze data from a broader perspective. The common aggregate functions are COUNT, SUM, AVG, MIN, and MAX. 
+
+For example, the aggregate function below calculates each product's total sales amount.
+*/
---
+++ database/MySQL/Tutorials/Window-Functions.sql
@@ -0,0 +1,10 @@
+/*
+Title: Window Functions
+
+Window Functions
+Window functions are special operations in SQL that allow you to perform calculations or 
+operations on a group of rows at once, considering their relationship within the group. 
+Common window functions include the LAG, RANK, DENSE_RANK, ROW_NUMBER, and many more. 
+
+For example, the ROW_NUMBER function can be used to rank customers by their maximum order amount. 
+*/
