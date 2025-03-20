8e636f26a7b4c0c7b47b8c7b1ae609a4396a0dc5
indresh535 <indreshindresh535@gmail.com>
2025-03-21 00:32:01 +0530

    sql advance concepts

---
+++ database/MySQL/Tutorials/QueryFilter.sql
@@ -0,0 +1,9 @@
+/*
+External Query Filter
+External query filters, predicate pushdown, or filter pushdown are optimization techniques to 
+improve query performance. They leverage the processing power of external data sources 
+(like another database or application) to filter data before it's transferred to your central SQL server. 
+
+In the example listed below, the external query filter (stored procedure in this case)
+ fetches the data from an external system.
+*/
