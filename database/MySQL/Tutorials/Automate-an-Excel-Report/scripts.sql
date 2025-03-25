7fa51944dbdca2007f5d3427ff8efeb218c14820
indresh535 <indreshindresh535@gmail.com>
2025-03-26 01:11:31 +0530

    autoamte schdule imoirt csv

---
+++ database/MySQL/Tutorials/Automate-an-Excel-Report/scripts.sql
@@ -0,0 +1,16 @@
+CREATE TABLE cust_orders (
+  cust_id INTEGER,
+  order_date DATE,
+  order_amount INTEGER
+);
+
+BULK INSERT dbo.cust_orders
+FROM 'C:\Users\Indre\Downloads\projects_run\DevDocs\database\MySQL\Tutorials\Automate-an-Excel-Report\cust_orders.csv'
+WITH
+(
+  FIRSTROW=2,
+  FIELDTERMINATOR =',',
+  ROWTERMINATOR = '0x0a'
+);
+
+select * from dbo.cust_orders
