1c00393033a3109644372bf07624ef1c0cc4f8aa
indresh535 <indreshindresh535@gmail.com>
2025-04-16 23:47:18 +0530

    Create_750m_records

---
+++ database/MySQL/Tutorials/generate_100millions_rows/Create_750m_records.sql
@@ -0,0 +1,29 @@
+Get-Content -Head 750000000 measurements.txt > measurements_750m.txt
+
+sh create_measurements.sh 500000000
+
+/*
+SQL Server commands to import the file 
+*/
+
+CREATE TABLE measurements (
+  city_name VARCHAR(500),
+  measurement VARCHAR(500)
+);
+
+
+BULK INSERT dbo.measurements
+FROM 'C:/Users/bbrum/databasestar/1brc/measurements.txt'
+WITH
+(
+  FIELDTERMINATOR =';',
+  ROWTERMINATOR = '0x0a'
+);
+
+SELECT
+city_name,
+MIN(measurement) AS min_temp,
+MAX(measurement) AS max_temp,
+AVG(measurement) AS avg_temp
+FROM measurements
+GROUP BY city_name;
