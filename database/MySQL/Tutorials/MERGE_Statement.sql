7699b13c826d9e0b4991719a8e76435d143c29ac
indresh535 <indreshindresh535@gmail.com>
2025-04-22 00:32:10 +0530

    merge statement sql

---
+++ database/MySQL/Tutorials/MERGE_Statement.sql
@@ -0,0 +1,8 @@
+MERGE statement in SQL is used to perform insert, update, and delete operations 
+on a target table based on the results of JOIN with a source table.
+ This allows users to synchronize two tables by performing operations on one table 
+ based on results from the second table.
+
+The MERGE statement compares data between a source table and a target table based on specified key fields. 
+It performs appropriate actions like inserting new records, updating existing ones, 
+and deleting or flagging records no longer present in the source
