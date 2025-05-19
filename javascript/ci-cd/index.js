f68bf93d6663cd77fd63455e547cde3ada571b9c
indresh535 <indreshindresh535@gmail.com>
2025-05-19 23:54:34 +0530

    deloy ci-cd yml

--- javascript/ci-cd/index.js
+++ javascript/ci-cd/index.js
@@ -8,7 +8,8 @@ app.use(express.json());
 // In-memory "database"
 let items = [
   { id: 1, name: "Item 1" },
-  { id: 2, name: "Item 2" }
+  { id: 2, name: "Item 2" },
+  { id: 3, name: "Item 3" }
 ];
 
 // GET all items
