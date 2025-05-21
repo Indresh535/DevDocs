8e1aceae1be46d6597fff0ae5844b84f1781c696
indresh535 <indreshindresh535@gmail.com>
2025-05-22 00:26:43 +0530

    deloy ci-cd yml

--- javascript/ci-cd/index.js
+++ javascript/ci-cd/index.js
@@ -11,7 +11,8 @@ let items = [
   { id: 2, name: "Item 2" },
   { id: 3, name: "Item 3" },
   { id: 4, name: "Item 4" },
-  { id: 5, name: "Item 5" }
+  { id: 5, name: "Item 5" },
+  { id: 6, name: "Item 6" }
 ];
 
 // GET all items
