50aeb4b1b07a6295d500c48b658ccadd2a92271e
indresh535 <indreshindresh535@gmail.com>
2025-06-13 01:12:58 +0530

    pyskark

--- python/Projects/pySpark/etl_job.py
+++ python/Projects/pySpark/etl_job.py
@@ -4,5 +4,4 @@ from pyspark.ml.regression import LinearRegression
 assembler = VectorAssembler(inputCols=["feature1", "feature2"], outputCol="features")
 data = assembler.transform(df)
 
-lr = LinearRegression(featuresCol="features", labelCol="label")
-model = lr.fit(data)
+lr = LinearRegression(featuresCol=
