ecd0040f67ba472d2320a8b8d293e14998c8ab8d
indresh535 <indreshindresh535@gmail.com>
2025-06-15 23:40:05 +0530

    setuping pyskark

---
+++ python/Projects/pySpark/config/spark_config.py
@@ -0,0 +1,9 @@
+from pyspark.sql import SparkSession
+
+def get_spark_session(app_name="PySparkApp", master="local[*]"):
+    spark = SparkSession.builder \
+        .appName(app_name) \
+        .master(master) \
+        .config("spark.sql.shuffle.partitions", 4) \
+        .getOrCreate()
+    return spark
